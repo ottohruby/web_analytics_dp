447150b8b53ece29be0b95067a217fd14a3c9bc3
Otto Hruby <xhruby27@vutbr.cz>
2023-10-03 16:44:12 +0200

    Terraform initial

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+provider "google" {
+  project = "${var.project_id}"
+  region = "${var.region}"
+}
 
+variable "gcp_required_apis"{
+  type = list(string)
+  default= [
+    "cloudresourcemanager.googleapis.com",
+    "apikeys.googleapis.com",
+    "compute.googleapis.com",
+    "sqladmin.googleapis.com",
+    "iam.googleapis.com"
+    ]
+}
+
+resource "google_project_service" "gcp_api" {
+  for_each = toset(var.gcp_required_apis)
+  project = data.gcp_project.project.project_id
+  service = each.key
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,9 @@
+variable "project_id" {
+    type = string
+    default = "otto-hruby-dp"
+}
+variable "region" {
+    type = string
+    default = "europe-west1"
+}
 
