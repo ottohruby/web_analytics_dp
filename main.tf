a738a831da07e1e5799600b51ba4685cb4b8017d
Otto Hruby <xhruby27@vutbr.cz>
2023-10-03 17:31:52 +0200

    typos

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "google" {
-    project = "${var.project_id}"
-    region = "${var.region}"
+    project = var.project_id
+    region = var.region
 }
 
 resource "google_project_service" "api" {
