c500a4cae9cd938634486164b26a87a8f6d5fea1
Otto Hruby <xhruby27@vutbr.cz>
2023-10-03 17:30:30 +0200

    fixed tf for data-logger

--- modules/data-logger/main.tf
+++ modules/data-logger/main.tf
@@ -4,14 +4,15 @@ resource "google_service_account" "service_account" {
 }
 
 resource "google_project_iam_member" "pubsub_publisher" {
-  project = "${var.project_id}"
+  project = var.project_id
   role    = "roles/pubsub.publisher"
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
 
 resource "google_cloud_run_v2_service" "data-logger" {
-  name     = "data-logger"
-  location = var.project_id
+  project = var.project_id
+  location = var.region
+  name = "data-logger"
 
   template {
     service_account = google_service_account.service_account.email
