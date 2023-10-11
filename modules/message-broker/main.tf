0ab0261b2ae707f6897cc7f6fd044b8f2e84fec9
Otto Hruby <xhruby27@vutbr.cz>
2023-10-11 21:02:33 +0200

    fixed bad var in schema path

--- modules/message-broker/main.tf
+++ modules/message-broker/main.tf
@@ -14,7 +14,7 @@ resource "google_pubsub_topic" "data-logger-events" {
 
   depends_on = [google_pubsub_schema.example]
   schema_settings {
-    schema = "projects/" + var.project_id + "/schemas/" + google_pubsub_schema.name
+    schema = "projects/" + var.project_id + "/schemas/" + google_pubsub_schema.data-logger-events.name
     encoding = "JSON"
   }
 
