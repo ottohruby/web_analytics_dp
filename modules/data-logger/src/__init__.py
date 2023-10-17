366cc35aed626331887050ec66cd554a7039b8e0
Otto Hruby <xhruby27@vutbr.cz>
2023-10-17 19:24:51 +0200

    , max_age=3600

--- modules/data-logger/src/__init__.py
+++ modules/data-logger/src/__init__.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
-    CORS(app)
+    CORS(app, max_age=3600)
 
     # Init extensions
     init_extensions(app)
