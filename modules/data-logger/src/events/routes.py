1c3861a4d22433feb6d466a5a9418e026e971262
Otto Hruby <xhruby27@vutbr.cz>
2023-10-25 19:23:27 +0200

    logger and clean up

--- modules/data-logger/src/events/routes.py
+++ modules/data-logger/src/events/routes.py
@@ -2,7 +2,50 @@ from flask import request, jsonify, request
 from src.events import bp
 from src.pubsub import publish_to_pubsub
 
-from datetime import datetime
+# todo check ts
+
+def fix_dims(dims):
+    req = [0, 2, 3, 6]
+    ids = dict()
+    fixed = []
+    for item in dims:
+        id = int(item['id'])
+        val = str(item['val'])
+        if ids.get(id):
+            raise ValueError(f"Dimension '{id}' is in list more than once!")
+        if id is not None and (val not in ('undefined', 'None')):
+            fixed.append({'id': id, 'val': val} )
+            ids[id] = 1
+
+    if(not ids.get(10)):
+        fixed.append({'id': 10, 'val': request.headers.get('User-Agent')} )
+
+    for id in req:
+        if not ids.get(id):
+            raise ValueError(f"Required dimension '{id}' is missing")
+    return fixed
+
+def fix_metrics(metrics):
+    req = [0]
+    ids = dict()
+    fixed = []
+    for item in metrics:
+        id = int(item['id'])
+        val = str(item['val'])
+        unit = int(item['unit'])
+        if ids.get(id):
+            raise ValueError(f"Dimension '{id}' is in list more than once!")
+        if id is not None and (val not in ('undefined', 'None')):
+            fixed.append({'id': id, 'val': val, 'unit': unit} )
+            ids[id] = 1
+
+    if(not ids.get(10)):
+        fixed.append({'id': 10, 'val': request.headers.get('User-Agent')} )
+
+    for id in req:
+        if not ids.get(id):
+            raise ValueError(f"Required dimension '{id}' is missing")
+    return fixed
 
 @bp.route('/collect/v1', methods=["POST", "GET"])
 def collect_v1():
@@ -13,21 +56,36 @@ def collect_v1():
         return jsonify({"error": "400 Bad request - JSON body with at least 1 event is required"}), 400
     
     request_json = request.get_json()
-    events = request_json.get("events")
+    print(request_json)
+    # events = request_json.get("events", [])
 
-    if not events:
-        return jsonify({"error": "400 Bad request - JSON body with at least 1 event is required"}), 400
+    if not request_json: # is instance of object
+        return jsonify({"error": "400 Bad request - Body is not JSON"}), 400
     
-    def process_event(event):
-        event['user_agent'] = request.headers.get('User-Agent')
-        if 'event_timestamp' not in event or 'event_date' not in event:
-            event["event_date"] = datetime.utcnow().strftime("%Y-%m-%d")
-            event['event_timestamp_micros'] = int(datetime.utcnow().timestamp() * 1000)
-
-        return event
+    event = {}
+    try:
+        event['ev_ts'] = request_json['ev_ts']
+        event['ev_id'] = int(request_json['ev_id'])
+        event['lg_id'] = int(request_json['lg_id'])
+        event['aw_id'] = int(request_json['aw_id'])
+        event['dims'] = request_json['dims']
+        event['metrics'] = request_json['metrics']
+    except KeyError as e:
+        return jsonify({"error": f"400 Bad request - {e} parameter is missing"}), 400
+    except ValueError as e:
+        return jsonify({"error": f"400 Bad request - {e} should be of type int"}), 400
     
-    for event in events:
-        processed_event = process_event(event)
-        publish_to_pubsub(processed_event)
+    if len(event['dims']) < 1:
+        return jsonify({"error": f"400 Bad request - event dims should have at least one param!"}), 400
+    
+    if len(event['metrics']) < 1:
+        return jsonify({"error": f"400 Bad request - event metrics should have at least one param!"}), 400
 
-    return jsonify({"processed data": request_json}), 200
+    try:
+        event['dims'] = fix_dims(event['dims'])
+        event['metrics'] = fix_metrics(event['metrics'])
+    except (ValueError, KeyError) as e:
+        return jsonify({"error": f"400 Bad request - {e}"}), 400
+    publish_to_pubsub(event)
+
+    return jsonify({"processed data": event}), 200
--- modules/data-viewer/app.py
+++
@@ -1,7 +0,0 @@
-from src import create_app
-from src.config import Config
-
-application = create_app()
-
-if __name__ == '__main__':
-  application.run(debug=True)
