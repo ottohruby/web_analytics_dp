f0b2258224ad91c95e6ee5a794defeed86f83228
ottohruby <70574289+ottohruby@users.noreply.github.com>
2024-03-17 14:06:14 +0100

    Update main.tf

--- modules/message-broker/main.tf
+++ modules/message-broker/main.tf
@@ -20,20 +20,6 @@ resource "google_pubsub_topic" "data-logger-events" {
 
 }
 
-resource "google_pubsub_topic_iam_binding" "binding" {
-  project = var.project_id
-  topic = google_pubsub_topic.data-logger-events.name
-  role = "roles/pubsub.publisher"
-
-  members = [
-    "allUsers"
-  ]
-
-  depends_on = [
-    google_pubsub_topic.data-logger-events
-  ]
-}
-
 resource "google_pubsub_subscription" "data-logger-events--pull--realtime" {
   project = var.project_id
   name  = "data-logger-events--pull--realtime"
