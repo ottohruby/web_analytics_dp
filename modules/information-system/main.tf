837c1e6334634cb9f1d3eac0710db3c743752be5
ottohruby <70574289+ottohruby@users.noreply.github.com>
2024-03-23 18:16:45 +0100

    Update main.tf

--- modules/information-system/main.tf
+++ modules/information-system/main.tf
@@ -52,7 +52,7 @@ resource "google_compute_instance" "information-system" {
   }
 
   service_account {
-    email  = google_service_account.service_account.email
+    email  = "${data.google_project.project.number}-compute@developer.gserviceaccount.com"
     scopes = ["https://www.googleapis.com/auth/cloud-platform", "https://www.googleapis.com/auth/devstorage.read_only", "https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/service.management.readonly", "https://www.googleapis.com/auth/servicecontrol", "https://www.googleapis.com/auth/trace.append"]
   }
 
