b2c741619b5c82ea1850b2cb7c4f67686a10e672
ottohruby <70574289+ottohruby@users.noreply.github.com>
2024-03-23 17:21:36 +0100

    Update variables.tf

--- modules/information-system/variables.tf
+++ modules/information-system/variables.tf
@@ -6,3 +6,15 @@ variable "region" {
     type = string
 }
 
+variable "sql_ip"{
+    type = string
+}
+
+variable "db_user"{
+    type = string
+}
+
+variable "db_password"{
+    type=string
+    sensitive = true
+}
