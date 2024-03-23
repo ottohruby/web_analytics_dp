2985652f0813ad998118275eb9dd16fd1b0079ef
ottohruby <70574289+ottohruby@users.noreply.github.com>
2024-03-23 17:02:11 +0100

    Update variables.tf

--- modules/postgresql/variables.tf
+++ modules/postgresql/variables.tf
@@ -6,16 +6,13 @@ variable "region" {
     type = string
 }
 
-variable "db_credentials" {
-  type = object({
-    db_user    = string
-    db_password = string
-  })
-  
-  default = {
-    db_user = "admin_user"
-    db_password = "admin_user123" 
-  }
-
-  sensitive = true
+variable "db_user"{
+    type = string
+    default = "admin_user"
 }
+
+variable "db_password"{
+    type=string
+    sensitive = true
+}
+
