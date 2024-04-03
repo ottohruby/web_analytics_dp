3b3fa0bcd37ff5664feaf3987be40ec7a44741b7
ottohruby <70574289+ottohruby@users.noreply.github.com>
2024-04-03 23:49:56 +0200

    added on delete cascade

--- modules/postgresql/tables/3_event_dimensions.sql
+++ modules/postgresql/tables/3_event_dimensions.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS analytics.event_dimensions CASCADE;
 CREATE TABLE analytics.event_dimensions (
     id serial PRIMARY KEY,
-    event_id integer REFERENCES analytics.event_stats(event_id),
+    event_id integer REFERENCES analytics.event_stats(event_id) ON DELETE CASCADE,
     dim_id integer REFERENCES analytics.dimensions(id),
     value varchar
 );
