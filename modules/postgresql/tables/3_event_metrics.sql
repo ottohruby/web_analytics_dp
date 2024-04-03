6776672852929e7c352964271871fd88355be15a
ottohruby <70574289+ottohruby@users.noreply.github.com>
2024-04-03 23:50:14 +0200

    added on delete cascade

--- modules/postgresql/tables/3_event_metrics.sql
+++ modules/postgresql/tables/3_event_metrics.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS analytics.event_metrics CASCADE;
 CREATE TABLE analytics.event_metrics (
     id serial PRIMARY KEY,
-    event_id integer REFERENCES analytics.event_stats(event_id),
+    event_id integer REFERENCES analytics.event_stats(event_id) ON DELETE CASCADE,
     metric_id integer REFERENCES analytics.metrics(id),
     unit_id integer REFERENCES analytics.units(id),
     function_id integer REFERENCES analytics.metric_functions(id),
