3c463d820e8b6cb2991e78e7f04ce04844f8b34c
ottohruby <70574289+ottohruby@users.noreply.github.com>
2024-04-08 23:19:26 +0200

    Update cron.sql - update every 15 s

--- modules/postgresql/functions/cron.sql
+++ modules/postgresql/functions/cron.sql
@@ -1,4 +1,4 @@
-SELECT cron.schedule('*/1 * * * *', 'DO $$ BEGIN PERFORM process_event_data(1); END $$;');
+SELECT cron.schedule('rt_stats','15 seconds', 'DO $$ BEGIN PERFORM process_event_data(1); END $$;');
 SELECT cron.schedule('2 */1 * * *', 'DO $$ BEGIN PERFORM process_event_data(2); END $$;');
 SELECT cron.schedule('5 1 * * *', 'DO $$ BEGIN PERFORM process_event_data(3); END $$;');
 SELECT cron.schedule('10 2 1 * *', 'DO $$ BEGIN PERFORM process_event_data(4); END $$;');
